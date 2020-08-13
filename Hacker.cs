27e2bb2ce853c8091e4479da7835f933d4426bd9
Caleb Cox <calebcox2010@gmail.com>
2020-08-12 21:43:08 -0500

    Update file
    Add level and level select function OnUnserInput()

--- Hacker.cs
+++ Hacker.cs
@@ -1,22 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+
+    // Game State
+    int level;
+
     // Start is called before the first frame update
     void Start()
     {
-        Terminal.WriteLine("Hello.");
-        Terminal.WriteLine("What would you like to hack into?");
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello, Human.");
+        Terminal.WriteLine("What would you like to hack with me?");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Enter your selection: ");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnUserInput(string input)
     {
-        
+        if(input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if(input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if(input == "411")
+        {
+            Terminal.WriteLine("What's the Disney 411?");
+        }
+        else
+        {
+            Terminal.WriteLine("Please enter a valid level");
+        }
     }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("You have chosen level " + level);
+    }
+
 }
